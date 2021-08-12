cdba0555ed1c9900bb7af1097fd35bc898e438d1
shielddd <88677908+shielddd@users.noreply.github.com>
2021-08-12 16:31:46 -0700

    Update shielddd.sh

--- shielddd.sh
+++ shielddd.sh
@@ -53,7 +53,7 @@ banner() {
 		${ORANGE}      | |                                
 		${ORANGE}      |_|                ${RED}Version : 2.1
 
-		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by htr-tech (tahmid.rayat)${WHITE}
+
 	EOF
 }
 
